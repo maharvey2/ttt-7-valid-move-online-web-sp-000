07ef113c3066a95f664710d4c307ae9bf7cc89f6
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-13 16:33:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,32 +1,16 @@
 # code your #valid_move? method here
-def valid_move(board,index)
-
+def valid_move(user_input, user_input = x)
+  user_input = gets.chomp
+  puts position_taken
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#def position_taken?(board,index)
-#  taken = nil
-#  if (board[index] == "" || board[index] == " " || board[index] == nil)
-#    taken = false
-#  else
-#    taken = true
-#  end
-#  taken
-#end
+def position_taken?(board,index)
+  taken = nil
+  if (board[index] == "" || board[index] == " " || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+  taken
+end
